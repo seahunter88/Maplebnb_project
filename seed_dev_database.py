bd22542f4f3c237d26d765f5685b47617fc65695
Tom <tomwight233@gmail.com>
2025-09-15 16:40:26 +0100

    added seed_dev_db.py

---
+++ seed_dev_database.py
@@ -0,0 +1,10 @@
+from lib.database_connection import DatabaseConnection
+
+# Run this file to reset your database using the seeds
+# ; python seed_dev_database.py
+
+connection = DatabaseConnection(test_mode=False)
+connection.connect()
+connection.seed("seeds/maplebnb.sql")
+# Add your own seed lines below...
+# E.g.connection.seed("seeds/your_seed.sql")
