560201519d5c4e5fe0f91f3e1a9442a867b04336
abdullah azhar <abdullahazhar@hotmail.co.uk>
2025-09-16 15:20:59 +0100

    test drive is_valid and generate_errors

--- lib/user.py
+++ lib/user.py
@@ -9,3 +9,21 @@ class User:
 
     def __repr__(self):
         return f'User({self.id}, {self.username}, {self.password})'
+
+    def is_valid(self):
+        if self.username == None or self.username == "":
+            return False
+        if self.password == None or self.password == "":
+            return False
+        return True
+
+    def generate_errors(self):
+        errors = []
+        if self.username == None or self.username == "":
+            errors.append("Username cannot be blank")
+        if self.password == None or self.password == "":
+            errors.append("Password cannot be blank")
+        if len(errors) == 0:
+            return None
+        else:
+            return ", ".join(errors)
