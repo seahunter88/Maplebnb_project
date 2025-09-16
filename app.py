47d9a8b9df472e77f79ddc91bf554527c9e20d9c
Jesse-Murray-Dev <209797357+Jesse-Murray-Dev@users.noreply.github.com>
2025-09-16 16:07:59 +0100

    Moved spaces HTML to spaces sub-directory

--- app.py
+++ app.py
@@ -26,14 +26,14 @@ def show_spaces():
     connection = get_flask_database_connection(app)
     repo = SpaceRepository(connection)
     spaces = repo.read_all_spaces()
-    return render_template('show_spaces.html', spaces = spaces)
+    return render_template('spaces/show_spaces.html', spaces = spaces)
 
 @app.route('/spaces/<int:space_id>', methods=['GET'])
 def show_one_space(space_id):
     connection = get_flask_database_connection(app)
     repo = SpaceRepository(connection)
     space = repo.read_one_space(space_id)
-    return render_template('show_one_space.html', space = space)
+    return render_template('spaces/show_one_space.html', space = space)
 
 @app.route('/signup', methods = ['POST'])
 def create_user():
