b29604fe3797b3eef775a424e61df701afa27105
Jesse-Murray-Dev <209797357+Jesse-Murray-Dev@users.noreply.github.com>
2025-09-16 12:05:04 +0100

    Created Repo Class for Spaces & read_all_spaces method with Tests

--- lib/space.py
+++ lib/space.py
@@ -1,16 +1,13 @@
 class Space():
-    def __init__(self, id, title, price, description):
+    def __init__(self, id, title, price, description, user_id):
         self.id = id
         self.title = title
         self.price = price
         self.description = description
-
-
+        self.user_id = user_id
 
     def __eq__(self,other):
         return self.__dict__ == other.__dict__  
     
     def __repr__(self):
-        return f"Space({self.id}, {self.title}, {self.price}, {self.description})"
-
-
+        return f"Space({self.id}, {self.title}, {self.price}, {self.description}, {self.user_id})"
--- tests/test_space.py
+++ tests/test_space.py
@@ -5,19 +5,20 @@ space initialises with id, title, price and description.
 '''
 
 def test_init():
-    space = Space(1, 'test space', 100, 'This is a test space.')
+    space = Space(1, 'test space', 100, 'This is a test space.', 1)
     assert space.id == 1
     assert space.title == 'test space'
     assert space.price == 100
     assert space.description == 'This is a test space.'
+    assert space.user_id == 1
 
 '''
 Testing that two instances of spaces are identical.
 '''
 
 def test_spaces_are_equal():
-    space = Space(1, 'test space', 100, 'This is a test space.')
-    space_2 = Space(1, 'test space', 100, 'This is a test space.')
+    space = Space(1, 'test space', 100, 'This is a test space.', 1)
+    space_2 = Space(1, 'test space', 100, 'This is a test space.', 1)
     assert space == space_2
 
 
@@ -26,5 +27,5 @@ Testing that space is formatting in a readable way.
 ''' 
 
 def test_make_it_a_string():
-    space = Space(1, 'test space', 100, 'This is a test space.')
-    assert str(space)== "Space(1, test space, 100, This is a test space.)"
+    space = Space(1, 'test space', 100, 'This is a test space.', 1)
+    assert str(space)== "Space(1, test space, 100, This is a test space., 1)"
