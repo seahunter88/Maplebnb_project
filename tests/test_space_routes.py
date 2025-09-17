35163b5dcd113f232bf4ec91151228c129beb041
Jesse-Murray-Dev <209797357+Jesse-Murray-Dev@users.noreply.github.com>
2025-09-17 11:54:38 +0100

    TEST[Space_Routes]: New Tests for returning form a specified space to all spaces

--- tests/test_space_routes.py
+++ tests/test_space_routes.py
@@ -66,3 +66,12 @@ def test_show_all_spaces_link_to_house_2(page, db_connection, test_web_address):
     expect(page.locator('h1')).to_have_text("House_2")
     expect(page.locator('h2')).to_have_text("Price per night: 150")
     expect(page.locator('h3')).to_have_text("Description: a nicer house")
+
+"""
+We can return from a specified space page back to show all spaces
+"""
+def test_return_to_show_all(page, db_connection, test_web_address):
+    db_connection.seed('seeds/maplebnb.sql')
+    page.goto(f'http://{test_web_address}/spaces/1')
+    page.click("text=Back to Spaces")
+    expect(page.locator('h1')).to_have_text("All Spaces")
