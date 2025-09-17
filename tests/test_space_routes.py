44105d5d576737acc0a8a7e371a584940435f039
Jesse-Murray-Dev <209797357+Jesse-Murray-Dev@users.noreply.github.com>
2025-09-17 11:34:11 +0100

    TEST[Space_Routes]: Added additional test for show specified space

--- tests/test_space_routes.py
+++ tests/test_space_routes.py
@@ -25,3 +25,13 @@ def test_get_first_space(page, db_connection, test_web_address):
     expect(h1_tag).to_have_text(["House_1"])
     expect(h2_tag).to_have_text(["Price per night: 100"])
     expect(h3_tag).to_have_text(["Description: a nice house"])
+
+def test_get_second_space(page, db_connection, test_web_address):
+    db_connection.seed('seeds/maplebnb.sql')
+    page.goto(f'http://{test_web_address}/spaces/2')
+    h1_tag = page.locator('h1')
+    h2_tag = page.locator('h2')
+    h3_tag = page.locator('h3')
+    expect(h1_tag).to_have_text(["House_2"])
+    expect(h2_tag).to_have_text(["Price per night: 150"])
+    expect(h3_tag).to_have_text(["Description: a nicer house"])
