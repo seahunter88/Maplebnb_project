4d58f7bcfe35b536a6e2a6dec23689d174aa98e4
Tom <tomwight233@gmail.com>
2025-09-17 16:30:17 +0100

    added link from booking_confirmation to spaces page

--- tests/test_booking_routes.py
+++ tests/test_booking_routes.py
@@ -32,7 +32,7 @@ def test_post_create_booking(page, test_web_address, db_connection):
 GET /booking_confirmation
   Parameters: None
   Expected response (200 OK):
-  "Create a booking!"
+  "Welcome to Maplebnb!"
 """
 def test_link_from_booking_confirmation_to_welcome_page(page, test_web_address, db_connection):
     page.set_default_timeout(1000)
@@ -42,7 +42,19 @@ def test_link_from_booking_confirmation_to_welcome_page(page, test_web_address, 
     page.click("text=Go to welcome page")
     expect(page.locator('h1')).to_have_text('Welcome to Maplebnb!')
 
-
+"""
+GET /booking_confirmation
+  Parameters: None
+  Expected response (200 OK):
+  "All Spaces"
+"""
+def test_link_from_booking_confirmation_to_spaces_page(page, test_web_address, db_connection):
+    page.set_default_timeout(1000)
+    db_connection.seed('seeds/maplebnb.sql')
+    page.goto(f'http://{test_web_address}/booking_confirmation')
+    expect(page.locator('h1')).to_have_text('Booking created!')
+    page.click("text=Go to spaces page")
+    expect(page.locator('h1')).to_have_text('All Spaces')
 
 
 # """
