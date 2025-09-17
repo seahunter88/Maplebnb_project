ad239069eb348a861be6ce037d37b1da4b017604
Jesse-Murray-Dev <209797357+Jesse-Murray-Dev@users.noreply.github.com>
2025-09-17 14:01:19 +0100

    FEAT[Space_Routes]: New route for create a space

--- space_routes.py
+++ space_routes.py
@@ -1,6 +1,7 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect
 from lib.database_connection import get_flask_database_connection
 from lib.space_repository import SpaceRepository
+from lib.space import Space
 
 def apply_space_routes(app):
     @app.route('/spaces', methods=['GET'])
@@ -16,3 +17,18 @@ def apply_space_routes(app):
         repo = SpaceRepository(connection)
         space = repo.read_one_space(space_id)
         return render_template('spaces/show_one_space.html', space = space)
+    
+    @app.route('/spaces/new', methods=['POST'])
+    def create_a_space():
+        connection = get_flask_database_connection(app)
+        repo = SpaceRepository(connection)
+        title = request.form['title']
+        price = request.form['price']
+        description = request.form['description']
+        user_id = request.form['user_id']
+        space = Space(None, title, price, description, user_id)
+        if not space.is_valid():
+            errors = space.generate_errors()
+            return render_template('spaces/new_space.html', error=errors)
+        repo.create_space(space)
+        return redirect('/spaces')
