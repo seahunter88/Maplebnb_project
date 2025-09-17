5f3967c57301dc11c08cd8004869007586b815c5
Jesse-Murray-Dev <209797357+Jesse-Murray-Dev@users.noreply.github.com>
2025-09-17 15:07:01 +0100

    FEAT[Space_Model]: added implementaion methods for validation

--- lib/space.py
+++ lib/space.py
@@ -12,8 +12,16 @@ class Space():
     def __repr__(self):
         return f"Space({self.id}, {self.title}, {self.price}, {self.description}, {self.user_id})"
     
-    def check_title_length(self):
+    def is_valid(self):
+        return self.check_title \
+            and self.check_price \
+            and self.check_description
+
+    def check_title(self):
         return len(self.title) >= 4 and len(self.title) <= 20
     
     def check_price(self):
-        return isinstance(self.price, int)
+        return isinstance(self.price, int) and self.price >= 20
+    
+    def check_description(self):
+        return isinstance(self.description, str) and len(self.description.split()) >= 3
