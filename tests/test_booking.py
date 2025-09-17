fc588a4fe87ea301031683e2f0ff89f0b7901f25
Tom <tomwight233@gmail.com>
2025-09-17 11:41:04 +0100

    added booking model class

---
+++ tests/test_booking.py
@@ -0,0 +1,30 @@
+from lib.booking import Booking
+
+'''
+When we construct a booking, it has an id, booking_date, booking_user_id and space_id,
+'''
+
+def test_booking_constructs_with_properties():
+    booking = Booking(1, '2025-09-17', 1, 1)
+    assert booking.id == 1
+    assert booking.booking_date == '2025-09-17'
+    assert booking.space_id == 1
+    assert booking.booking_user_id == 1
+    
+
+'''
+booking objects are treated equally if they have equal values
+'''
+
+def test_equality():
+    booking_1 = Booking(1, '2025-09-17', 1, 1)
+    booking_2 = Booking(1, '2025-09-17', 1, 1)
+    assert booking_1 == booking_2
+
+'''
+booking objects are formatted as a nice string
+'''
+
+def test_booking_formats_nicely_as_a_string():
+    booking_1 = Booking(1, '2025-09-17', 1, 1)
+    assert str(booking_1) == 'Booking(1, 2025-09-17, 1, 1)'
