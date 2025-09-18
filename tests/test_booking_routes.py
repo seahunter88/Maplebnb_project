9b580c9cd65eb9f8256c3fdf0a71e46ab368982b
Tom <tomwight233@gmail.com>
2025-09-18 16:49:23 +0100

    added class tag to form in welcome.html

--- tests/test_booking_routes.py
+++ tests/test_booking_routes.py
@@ -199,3 +199,33 @@ def test_duplicate_booking_is_not_redirected_2(page, test_web_address, db_connec
     expect(page.locator('h1')).to_have_text('House_2')
     expect(page.locator('h5')).to_have_text('2025-08-17')
     expect(page.locator('.t-errors')).to_have_text('Unfortunately this space is being used for a Pokemon convention on that date, please try a different date.')
+    
+"""
+When I try to create a duplicate booking, 
+it re-renders the show_one_space page, does not redirect to booking_confirmation
+This duplicate booking is NOT shown in the list of unavailable dates
+"""
+def test_duplicate_booking_is_not_redirected(page, test_web_address, db_connection):
+    page.set_default_timeout(1000)
+    db_connection.seed('seeds/maplebnb.sql')
+    page.goto(f'http://{test_web_address}/spaces/1')
+    expect(page.locator('h1')).to_have_text('House_1')
+    page.fill("input[name=booking_date]", '2025-09-17')
+    page.fill("input[name=booking_user_id]", '1')
+    page.click("text=Create booking")
+    expect(page.locator('h1')).to_have_text('House_1')
+    expect(page.locator('h5')).to_have_text('2025-09-17')
+    expect(page.locator('.t-errors')).to_have_text('Unfortunately this space is being used for a Pokemon convention on that date, please try a different date.')
+
+
+"""
+When I visit the create a booking form on show_one_space
+it does NOT have the error message we would get from creating a duplicate booking
+"""
+def test_show_one_space_does_not_have_error_message(page, test_web_address, db_connection):
+    page.set_default_timeout(1000)
+    db_connection.seed('seeds/maplebnb.sql')
+    page.goto(f'http://{test_web_address}/spaces/2')
+    expect(page.locator('h1')).to_have_text('House_2')
+    # expect(page.locator('.t-errors')).not.toContainText('Unfortunately this space is being used for a Pokemon convention on that date, please try a different date.')
+    
