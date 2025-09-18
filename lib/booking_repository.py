14f26db8d8aed24283a8e4b62d71242ea0a9c274
Tom <tomwight233@gmail.com>
2025-09-18 13:04:23 +0100

    finished test driving the create in booking_repo with no duplicates

--- lib/booking_repository.py
+++ lib/booking_repository.py
@@ -12,27 +12,16 @@ class BookingRepository:
             ]
         
     def create(self, booking):
-        if self.check_booking_is_unique()
-        table = self._connection.execute("INSERT INTO bookings (booking_date, space_id, booking_user_id) VALUES (%s, %s, %s) RETURNING id", [booking.booking_date, booking.space_id, booking.booking_user_id])
-        row = table[0]
-        booking.id = row['id']
-        return None
+        if self.check_booking_is_unique(booking):
+            table = self._connection.execute("INSERT INTO bookings (booking_date, space_id, booking_user_id) VALUES (%s, %s, %s) RETURNING id", [booking.booking_date, booking.space_id, booking.booking_user_id])
+            row = table[0]
+            booking.id = row['id']
+            return None
+        return "Unfortunately this space is being used for a Pokemon convention on that date, please try a different date."
     
     def check_booking_is_unique(self, booking):
         table = self._connection.execute("SELECT * FROM bookings WHERE booking_date = %s AND space_id = %s", [booking.booking_date, booking.space_id])
         return table == []
-    
-    # def create(self, user):
-    #     if self.check_username_is_unique(user.username):
-    #         table = self._connection.execute("INSERT INTO users (username, password) VALUES (%s, %s) RETURNING id", [user.username, user.password])
-    #         row = table[0]
-    #         user.id = row['id']
-    #         return None
-    #     return "Username is already in use."
-    
-    # def check_username_is_unique(self, username):
-    #     table = self._connection.execute("SELECT * FROM users WHERE username = %s", [username])
-    #     return table == []
     
     def read_bookings_one_space(self, space_id):
         table = self._connection.execute("SELECT * FROM bookings WHERE space_id = %s", [space_id])
--- tests/test_booking_repository.py
+++ tests/test_booking_repository.py
@@ -197,7 +197,7 @@ def test_create_bookings_uses_check_booking_unique(db_connection):
     db_connection.seed('seeds/maplebnb.sql')
     repo = BookingRepository(db_connection)
     booking_1 = Booking(3, '2025-09-17', 1, 1)
-    repo.create(booking_1)
+    assert repo.create(booking_1) == "Unfortunately this space is being used for a Pokemon convention on that date, please try a different date."
     assert repo.check_booking_is_unique(booking_1) == False
     assert repo.all() == [
         Booking(1, date(2025, 9, 17), 1, 1),
